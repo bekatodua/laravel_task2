a8ade9270dd39020aa1cd62593a22d4640acd9b7
bekatodua <beka.todua.1@btu.edu.ge>
2021-11-24 16:06:45 +0400

    added controller and blades

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function showBlog(Request $request) {
+
+    }
+
+    public function showPost(Request $request) {
+
+    }
+}
